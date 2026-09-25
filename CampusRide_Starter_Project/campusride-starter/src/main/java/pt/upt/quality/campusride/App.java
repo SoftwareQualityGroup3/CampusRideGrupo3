34de5b53e622f834e55665145ab55765a1f1479b
Guilherme da Costa Ribeiro <gcostaribeiro2006@gmail.com>
2026-09-25 15:48:14 +0100

    Remove placeholder message for team features

--- CampusRide_Starter_Project/campusride-starter/src/main/java/pt/upt/quality/campusride/App.java
+++ CampusRide_Starter_Project/campusride-starter/src/main/java/pt/upt/quality/campusride/App.java
@@ -20,6 +20,5 @@ public class App {
         EBike eBike = new EBike("E20", 95);
         eBike.charge(20);
         System.out.println("E20 battery = " + eBike.getBatteryLevel());
-        System.out.println("TEAM FEATURES NOT YET INTEGRATED");
     }
 }
